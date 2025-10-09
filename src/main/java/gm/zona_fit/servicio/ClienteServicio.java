8238dfe7bfb2f1ebbc76dd185c656bcb09bf820c
WillmanGZ <willmang@uninorte.edu.co>
2025-10-09 12:31:18 -0500

    feat: crear el servicio para clientes implementando la interfaz antes definida

---
+++ src/main/java/gm/zona_fit/servicio/ClienteServicio.java
@@ -0,0 +1,37 @@
+package gm.zona_fit.servicio;
+
+import gm.zona_fit.modelo.Cliente;
+import gm.zona_fit.repositorio.ClienteRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClienteServicio implements IClienteServicio {
+    // Usamos AutoWired para usar injeccion de dependencias
+    // Aunque sea una interfaz, posteriormete será clase asi que podemos usar sus metodos cuando lo necesitemos
+    @Autowired
+    private ClienteRepositorio clienteRepositorio;
+
+    @Override
+    public List<Cliente> listarClientes() {
+        return clienteRepositorio.findAll();
+    }
+
+    @Override
+    public Cliente buscarClientePorId(Integer idCliente) {
+        // El metodo findbyid retorna un optional, por lo que podemos poner un else null en caso de que no encuentre al cliente
+        return clienteRepositorio.findById(idCliente).orElse(null);
+    }
+
+    @Override
+    public void guardarCliente(Cliente cliente) {
+        clienteRepositorio.save(cliente);
+    }
+
+    @Override
+    public void eliminarCliente(Cliente cliente) {
+        clienteRepositorio.delete(cliente);
+    }
+}
--- src/main/java/gm/zona_fit/servicio/IClienteServicio.java
+++ src/main/java/gm/zona_fit/servicio/IClienteServicio.java
@@ -7,7 +7,7 @@ import java.util.List;
 public interface IClienteServicio {
     public List<Cliente> listarClientes();
 
-    public Cliente buscarClientePorId(Cliente cliente);
+    public Cliente buscarClientePorId(Integer idCliente);
 
     public void guardarCliente(Cliente cliente);
 
