abebf998b072cdd4a92803e79bb4811a0468d33e
WillmanGZ <willmang@uninorte.edu.co>
2025-10-11 14:20:41 -0500

    feat: agregar las cabeceras de las tablas

--- src/main/java/gm/zona_fit/gui/ZonaFitForma.java
+++ src/main/java/gm/zona_fit/gui/ZonaFitForma.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
 @Component
 public class ZonaFitForma extends JFrame {
     private JPanel panelPrincipal;
     private JTable clientesTabla;
     IClienteServicio clienteServicio;
+    private DefaultTableModel tablaModeloClientes;
 
     @Autowired
     public ZonaFitForma(ClienteServicio clienteServicio) {
@@ -24,5 +26,13 @@ public class ZonaFitForma extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(900, 700);
         setLocationRelativeTo(null);
+    }
+
+    private void createUIComponents() {
+        this.tablaModeloClientes = new DefaultTableModel(0, 4);
+        String[] cabeceros = {"Id", "Nombre", "Apellido", "Membresía"};
+        this.tablaModeloClientes.setColumnIdentifiers(cabeceros);
+        this.clientesTabla = new JTable(tablaModeloClientes);
+
     }
 }
